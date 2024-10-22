10f1ef0de4d62dab2892c7f56142a73ada430270
James Maroko <jamiemaroko41@gmail.com>
2024-10-22 10:38:31 +0300

    Updated rates

--- code/challenge3.js
+++ code/challenge3.js
@@ -1,23 +1,22 @@
 function calculateNetSalary(basicSalary, benefits) {
-  // Replace with actual KRA, NHIF, and NSSF rates obtained from a reliable source
-  const PAYE_RATE = 0.3; // Hypothetical PAYE rate
-  const NHIF_RATE = 0.05; // Hypothetical NHIF rate
-  const NSSF_RATE = 0.06; // Hypothetical NSSF rate (employer and employee contribution)
+  const PAYE_RATE = 0.1; // Up to 288,000
+  const SHIF_RATE = 0.0275; // effective 1st October
+  const NSSF_RATE = 0.06; // employer and employee contribution
 
-  // Calculate gross salary, PAYE, NHIF deductions, and NSSF deductions
+  // Calculate gross salary, PAYE, SHIF deductions, and NSSF deductions
   const grossSalary = basicSalary + benefits;
   const payee = grossSalary * PAYE_RATE;
-  const nhifDeductions = grossSalary * NHIF_RATE;
+  const shifDeductions = grossSalary * SHIF_RATE;
   const nssfDeductions = grossSalary * NSSF_RATE;
 
   // Calculate net salary
-  const netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
+  const netSalary = grossSalary - payee - shifDeductions - nssfDeductions;
 
   // Create an object to store the results
   const results = {
     grossSalary,
     payee,
-    nhifDeductions,
+    shifDeductions,
     nssfDeductions,
     netSalary,
   };
@@ -32,6 +31,6 @@ const netSalaryInfo = calculateNetSalary(basicSalary, benefits);
 
 console.log("Gross Salary:", netSalaryInfo.grossSalary);
 console.log("PAYE:", netSalaryInfo.payee);
-console.log("NHIF Deductions:", netSalaryInfo.nhifDeductions);
+console.log("SHIF Deductions:", netSalaryInfo.shifDeductions);
 console.log("NSSF Deductions:", netSalaryInfo.nssfDeductions);
 console.log("Net Salary:", netSalaryInfo.netSalary);
